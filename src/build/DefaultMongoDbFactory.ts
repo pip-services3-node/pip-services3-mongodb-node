b5ba0946d17f4c41805732b2c3aad6fb44bc25b6
banalna <judas.priest999@gmail.com>
2020-11-25 16:14:45 +0300

    fix links in docs and package.json

--- src/build/DefaultMongoDbFactory.ts
+++ src/build/DefaultMongoDbFactory.ts
@@ -7,7 +7,7 @@ import { MongoDbConnection } from '../persistence/MongoDbConnection';
 /**
  * Creates MongoDb components by their descriptors.
  * 
- * @see [[https://rawgit.com/pip-services-node/pip-services3-components-node/master/doc/api/classes/build.factory.html Factory]]
+ * @see [[https://pip-services3-node.github.io/pip-services3-components-node/classes/build.factory.html Factory]]
  * @see [[MongoDbConnection]]
  */
 export class DefaultMongoDbFactory extends Factory {
